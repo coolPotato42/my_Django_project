9904ee08f1e793cbd2c243bf16ad751030cfde50
Sihionok <Sihionok@gmail.com>
2022-11-19 16:48:04 +0300

    dobavel novie modeli

--- lms/admin.py
+++ lms/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from lms.models import Curator, Direction
+from lms.models import Curator, Direction, Group, Disziplina
 
 admin.site.register(Curator)
 admin.site.register(Direction)
+admin.site.register(Group)
+admin.site.register(Disziplina)
--- lms/models.py
+++ lms/models.py
@@ -9,3 +9,16 @@ class Curator(models.Model):
 class Direction(models.Model):
     name = models.CharField(max_length=20)
     curator = models.ForeignKey(Curator, on_delete=models.CASCADE, related_name='direction')
+
+class Disziplina(models.Model):
+    name = models.CharField(max_length=20)
+    Direction = models.ForeignKey(Disziplina, on_delete=models.CASCADE, related_name='disziplina')
+
+class Group(models.Model):
+    name = models.CharField(max_length=20)
+    created_date = models.DateTimeField(default=timezone.now)
+    direction = models.ForeignKey(Direction, on_delete=models.CASCADE, related_name='group')
+
+class Student(models.Model):
+    name = name = models.CharField(max_length=20)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='student')
